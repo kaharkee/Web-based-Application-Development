92b601e75346fdd3fcf92601e304b64221db7240
Harkeerat kaur <98957720+kaharkee@users.noreply.github.com>
2024-07-17 17:00:49 -0400

    Add files via upload

---
+++ Assignment 3/src/src/app/myClasses/kaharkee.ts
@@ -0,0 +1,11 @@
+export class Kaharkee {
+  Fname!: string;
+  Lname!: string;
+  Sid!: number;
+  Username!: string;
+  Progname!: string;
+  Semail!: string;
+  Hcampus!: string;
+  City!: string;
+  Picname!: string;
+}
